0ad63b747415ff0fb9039d61082ca26b7158c903
pianonerd7 <jxh604@case.edu>
2016-01-04 12:22:46 -0500

    gets html

--- App_Code/Utilities/HTMLAnalyzer.cs
+++ App_Code/Utilities/HTMLAnalyzer.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 
 /// <summary>
@@ -8,10 +11,32 @@ using System.Web;
 /// </summary>
 public class HTMLAnalyzer
 {
-	public HTMLAnalyzer()
-	{
-		//
-		// TODO: Add constructor logic here
-		//
-	}
+
+    public static string URLToHTML(string url)
+    {
+        String http = null;
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            Stream receiveStream = response.GetResponseStream();
+            StreamReader readStream = null;
+
+            if (response.CharacterSet == null)
+            {
+                readStream = new StreamReader(receiveStream);
+            }
+            else
+            {
+                readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+            }
+            http = readStream.ReadToEnd();
+
+            response.Close();
+            readStream.Close();
+        }
+        return http;
+    }
 }
