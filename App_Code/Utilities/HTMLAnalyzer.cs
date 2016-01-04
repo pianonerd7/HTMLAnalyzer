d95283faa0cc6049bac1fc30885cd95f3fc0c475
pianonerd7 <jxh604@case.edu>
2016-01-04 11:59:47 -0500

    added html analyzer

---
+++ App_Code/Utilities/HTMLAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for HTMLAnalyzer
+/// </summary>
+public class HTMLAnalyzer
+{
+	public HTMLAnalyzer()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
