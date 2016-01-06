d1cf54346c3da475dd8bbf185da2ac75dffd590b
pianonerd7 <jxh604@case.edu>
2016-01-06 09:57:54 -0500

    added edge case check. need more

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -12,7 +12,6 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
-    public List<string> htmlTags = new List<string>();
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -21,25 +20,50 @@ public partial class _Default : System.Web.UI.Page
     protected void Execute_Analyze_Command_Click(object sender, EventArgs e)
     {
         string url = urlpath.Value;
-        summarylabel.Text = "HTML Tag Summary";
-        htmlcodelabel.Text = "HTML Source Code";
 
-        htmlTags.Add("list1");
-        htmlTags.Add("list2");
+        if (url.Length > 0)
+        {
+            summarylabel.Text = "HTML Tag Summary";
+            htmlcodelabel.Text = "HTML Source Code";
 
+            HTMLAnalyzer analyzer = new HTMLAnalyzer(url);
 
-        HTMLAnalyzer analyzer = new HTMLAnalyzer(url);
+            tagsummary.DataSource = analyzer.CountTags();
+            tagsummary.DataBind();
 
-        tagsummary.DataSource = analyzer.CountTags();
-        tagsummary.DataBind();
-
-        htmlsourcecode.Text = Server.HtmlEncode(analyzer.getHtml());
+            htmlsourcecode.Text = Server.HtmlEncode(analyzer.getHtml());
+        }
+        //TODO add try catch for invalid url
     }
 
     protected void tagsummary_rowCommand(object sender, EventArgs e)
     {
         GridViewRow row = (GridViewRow)((Button)sender).NamingContainer;
         string tag = row.Cells[1].Text;
+        //htmlsourcecode.Text = tag;
 
+        htmlsourcecode.Text = HighlightKeywords("html", tag);
+    }
+
+    public string HighlightKeywords(string input, string keywords)
+    {
+        if (input == string.Empty || keywords == string.Empty)
+        {
+            return input;
+        }
+
+        string[] sKeywords = keywords.Split(' ');
+        foreach (string sKeyword in sKeywords)
+        {
+            try
+            {
+                input = Regex.Replace(input, sKeyword, string.Format("<span class=\"hit\">{0}</span>", "$0"), RegexOptions.IgnoreCase);
+            }
+            catch
+            {
+                //
+            }
+        }
+        return input;
     }
 }
